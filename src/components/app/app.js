f237061ee7514aff06467bcb2b7e8974c27198d4
perian <dmperian@gmail.com>
2022-02-07 00:53:14 +0200

    Рефакторинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,14 @@
 
 import { Route, Routes } from 'react-router-dom';
-import { Tournament } from '../tournament-list/tournament';
-import Main from './main';
+import { TournamentList } from '../tournament-list/tournament-list';
+import { Tournament } from '../tournament-list/tournament-settings';
 import ErrorPage from '../error-page/errorPage'
-import './app';
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route index element={<Main />} />
+        <Route index element={<TournamentList />} />
         <Route path="tournament/:id" element={<Tournament />} />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
--- src/components/tournament-list/tournament-list.js
+++ src/components/tournament-list/tournament-list.js
@@ -1,19 +1,55 @@
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { fetchTournaments } from '../data-service/data-service'
+import { useEffect, useState } from 'react';
 import { TournamentItem } from "./tournament-item"
 import './tournament-list.css';
 
 export const TournamentList = () => {
+  const dispatch = useDispatch();
   const tournaments = useSelector(state => state.tournaments.all);
+  const isOutOfData = useSelector(state => state.tournaments.dataLimit);
+  const [pageCount, setPageCount] = useState(1);
+
+  useEffect(() => {
+    dispatch(fetchTournaments(`&page=1`,tournaments));
+    setPageCount(pageCount + 1);
+  }, []);
+
+  const getMoreTournaments = () => {
+    dispatch(fetchTournaments(`&page=${pageCount}`, tournaments));
+    setPageCount(pageCount + 1);
+  }
+
+  const List = () => {
+    return (
+      <React.Fragment>
+        <ul className="tournaments-list">
+          {tournaments.map(tournament => 
+            <TournamentItem tournament={tournament} 
+                            key={tournament.id} 
+            />
+          )}
+        </ul>
+        <div className='d-grid gap-2 col-3 mx-auto'>
+          <button className='btn btn-primary mb-3' 
+                  onClick={() => getMoreTournaments()} disabled={isOutOfData}
+          >
+          Need MORE tournaments!
+          </button>
+        </div>
+      </React.Fragment>
+    )
+  }
 
   return (
     <section className="container">
-      <ul className="tournaments-list">
-        {tournaments.map(tournament => 
-          <TournamentItem tournament={tournament} 
-                          key={tournament.id} 
-          />
-        )}
-      </ul>
+      {tournaments.length
+        ?
+        <List/>
+        :
+        <div>Турниры отсутствуют</div>
+      }
     </section>
   )
 }
