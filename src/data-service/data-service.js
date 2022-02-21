027125515cc6434b0b5a53002c79b06035911438
perian <dmperian@gmail.com>
2022-02-21 05:28:20 +0200

    Исправил получение сообщений о ошибке

--- src/data-service/data-service.js
+++ src/data-service/data-service.js
@@ -8,39 +8,39 @@ import axios from "axios";
 const _baseUrl = `https://api.gamestars.tv/streamly/v2/tournaments`;
 
 export const fetchTournaments = () => {
-  try {
-    return async function (dispatch) {
+  return async (dispatch) => {
+    try {
       const response = await axios.get(`${_baseUrl}?game=pubg4x4mobile&page=1`);
 
       dispatch(getTournamentsAction(response.data));
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
 export const fetchMoreTournaments = (page) => {
-  try {
-    return async function (dispatch) {
+  return async (dispatch) => {
+    try {
       const response = await axios.get(
         `${_baseUrl}?game=pubg4x4mobile&page=${page}`
       );
 
       dispatch(getMoreTournamentsAction(response.data));
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
 export const fetchTournamentSettings = (id) => {
-  try {
-    return async function (dispatch) {
+  return async (dispatch) => {
+    try {
       const response = await axios.get(`${_baseUrl}/${id}/settings`);
 
       dispatch(getTournamentSettingsAction(response.data));
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
